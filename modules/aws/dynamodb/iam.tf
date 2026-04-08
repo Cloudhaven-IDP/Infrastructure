75ad3c35206a2986ece10125d16df9e36ef64987
Afolabi Fajobi <afolabi.fajobi1@yahoo.com>
2026-04-07 21:38:37 -0400

    add dynamodb module
    Signed-off-by: Afolabi Fajobi <afolabi.fajobi1@yahoo.com>

---
+++ modules/aws/dynamodb/iam.tf
@@ -0,0 +1,62 @@
+data "aws_caller_identity" "current" {}
+
+#------------------------------------------------------------------------------
+# Read policy
+#------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "read" {
+  statement {
+    sid    = "DynamoDBReadAccess"
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:DescribeTable",
+    ]
+
+    resources = [
+      module.dynamodb_table.dynamodb_table_arn,
+      "${module.dynamodb_table.dynamodb_table_arn}/index/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "read" {
+  name        = "${local.table_name}-read"
+  description = "Read access to the ${local.table_name} DynamoDB table"
+  policy      = data.aws_iam_policy_document.read.json
+  tags        = local.tags
+}
+
+#------------------------------------------------------------------------------
+# Write policy
+#------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "write" {
+  statement {
+    sid    = "DynamoDBWriteAccess"
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:BatchWriteItem",
+    ]
+
+    resources = [
+      module.dynamodb_table.dynamodb_table_arn,
+      "${module.dynamodb_table.dynamodb_table_arn}/index/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "write" {
+  name        = "${local.table_name}-write"
+  description = "Write access to the ${local.table_name} DynamoDB table"
+  policy      = data.aws_iam_policy_document.write.json
+  tags        = local.tags
+}

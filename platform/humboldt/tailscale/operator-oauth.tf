3d963639e80c96a1aeed0bc89187ffefd750c866
Afolabi Fajobi <afolabi.fajobi1@yahoo.com>
2026-04-19 19:51:51 -0400

    fix tailscale acl to allow envoy proxy for Argocd path
    Signed-off-by: Afolabi Fajobi <afolabi.fajobi1@yahoo.com>

--- platform/humboldt/tailscale/operator-oauth.tf
+++ platform/humboldt/tailscale/operator-oauth.tf
@@ -1,7 +1,7 @@
 resource "tailscale_oauth_client" "humboldt_operator" {
   description = "Tailscale Kubernetes operator - humboldt cluster"
   scopes      = ["devices"]
-  tags        = ["tag:k8s-humboldt"]
+  tags        = ["tag:k8s-worker"]
 }
 
 resource "aws_ssm_parameter" "operator_client_id" {

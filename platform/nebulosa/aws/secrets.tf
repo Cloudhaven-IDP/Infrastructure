4acfcd72c0d82a30f74dfc1ae57801ac919680a3
Afolabi Fajobi <afolabi.fajobi1@yahoo.com>
2026-04-30 08:37:11 -0400

    init cluster kubeconfig
    Signed-off-by: Afolabi Fajobi <afolabi.fajobi1@yahoo.com>

--- platform/nebulosa/aws/secrets.tf
+++ platform/nebulosa/aws/secrets.tf
@@ -3,3 +3,9 @@ module "langfuse_secret" {
   name        = "nebulosa/langfuse"
   description = "Langfuse stack secrets"
 }
+
+module "nebulosa_kubeconfig" {
+  source      = "../../../modules/aws/secrets-manager"
+  name        = "nebulosa/kubeconfig"
+  description = "Full kubeconfig YAML for the nebulosa cluster (admin@nebulosa context). Value managed by scripts/aws/migrate-kubeconfigs-to-secrets-manager.sh."
+}
